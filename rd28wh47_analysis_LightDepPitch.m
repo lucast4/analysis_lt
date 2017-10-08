84b9b2f29824db273186bd5719aabafe3507d717
lucast4 <lucas.tian@ucsf.edu>
2017-10-08 12:58:39 -0700

    stuff

---
+++ rd28wh47_analysis_LightDepPitch.m
@@ -0,0 +1,20 @@
+%% CHECKING TEMPLATE MATCHING;
+
+clear all; close all;
+batchf='batch.rand.keep.LABELED';
+batchf='batch.labeled.all';
+config='/bluejay5/lucas/birds/rd28wh47/config_100217_2.evconfig2'; % first config
+config='/bluejay5/lucas/birds/rd28wh47/config_100617.evconfig2'; % starte 10/6
+
+% === WN (d[h])
+syl='h';
+syl_pre='d';
+syl_post='';
+get_WN_hits=1;
+get_offline_match=1;
+get_FF=1;
+NoteNum=0;
+
+check_stuff=lt_check_hit_templ_freq_v2_EvTAFv4Sim(batchf, syl, syl_pre, syl_post, get_WN_hits,get_offline_match,get_FF,config,NoteNum);
+
+
