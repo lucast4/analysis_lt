34f223afbbdaf5554b85071d83925f12638e1fe3
lucast4 <lucas.tian@ucsf.edu>
2017-10-28 14:42:37 -0700

    stuff

--- pu46wh04_analysis_LightDepPitch.m
+++ pu46wh04_analysis_LightDepPitch.m
@@ -5,6 +5,8 @@ clear all; close all;
 batchf='batch.labeled.all';
 % config='/bluejay5/lucas/birds/rd28wh47/config_100217_2.evconfig2'; % first config
 config='/bluejay5/lucas/birds/pu46wh04/config101817.evconfig2'; %
+config='/bluejay5/lucas/birds/pu46wh04/config102217.evconfig2'; %
+config='/bluejay5/lucas/birds/pu46wh04/config102617.evconfig2'; %
 
 % === WN (d[h])
 syl='b';
@@ -13,11 +15,11 @@ syl_post='';
 get_WN_hits=1;
 get_offline_match=1;
 get_FF=1;
-NoteNum=0;
+NoteNum=1;
 
-% % --- temp (testing template)
-% config='/bluejay5/lucas/birds/rd28wh47/config_101217_onenote.evconfig2'; %
-% NoteNum=0;
+% --- temp (testing template)
+config='/bluejay5/lucas/birds/pu46wh04/configtest.evconfig2'; %
+NoteNum=0;
 
 check_stuff=lt_check_hit_templ_freq_v2_EvTAFv4Sim(batchf, syl, syl_pre, syl_post, get_WN_hits,get_offline_match,get_FF,config,NoteNum);
 
--- rd28wh47_analysis_LightDepPitch.m
+++ rd28wh47_analysis_LightDepPitch.m
@@ -7,6 +7,7 @@ batchf='batch.labeled.all';
 config='/bluejay5/lucas/birds/rd28wh47/config_100617.evconfig2'; % starte 10/6
 config='/bluejay5/lucas/birds/rd28wh47/config_100917.evconfig2'; %
 config='/bluejay5/lucas/birds/rd28wh47/config_101317.evconfig2'; %
+config='/bluejay5/lucas/birds/rd28wh47/config_102317.evconfig2'; %
 
 % === WN (d[h])
 syl='h';
@@ -18,8 +19,9 @@ get_FF=1;
 NoteNum=1;
 
 % % --- temp (testing template)
-% config='/bluejay5/lucas/birds/rd28wh47/config_101217_onenote.evconfig2'; %
-% NoteNum=0;
+config='/bluejay5/lucas/birds/rd28wh47/config_template.evconfig2'; %
+config='/bluejay5/lucas/birds/rd28wh47/config_template2.evconfig2'; %
+NoteNum=0;
 
 check_stuff=lt_check_hit_templ_freq_v2_EvTAFv4Sim(batchf, syl, syl_pre, syl_post, get_WN_hits,get_offline_match,get_FF,config,NoteNum);
 
